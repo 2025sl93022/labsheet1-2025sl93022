b32d2f55c5d794ef6632e09d2978661831976519
2025sl93022 <2025sl93022@wilp.bits-pilani.ac.in>
2026-04-04 12:17:14 +0530

    Added divide function

--- calculator.py
+++ calculator.py
@@ -4,6 +4,13 @@ def add(a, b):
 def multiply(a, b):
 	return a*b
 
+<<<<<<< HEAD
 def subtract(a, b):
 	return a-b
+=======
+def divide(a, b):
+	if b==0:
+		return None
+	return a/b
+>>>>>>> 5ccf443 (Added divide function)
 
